5c97dcffa81a994c8af221b783cb7241d65a3d4e
=Nikodem Smoliński <=n.smolinski.089@studms.ug.edu.pl>
2025-11-11 13:06:28 +0100

    proporcjonalny resize Main zoom

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,7 +2,7 @@ import { Settings, User, BusFront } from 'lucide-react';
 
 export default function Main() {
   return (
-    <div className="bg-white w-1/3 h-4/5 m-6 rounded-lg shadow-lg">
+    <div className="bg-white w-auto h-auto min-w-196 min-h-196 max-w-2xl max-h-[800px] m-6 rounded-lg shadow-lg">
         <div className="flex flex-row items-center justify-between h-1/12 p-2 border-b border-gray-100">
           <button className="p-0 w-fit h-fit hover:opacity-70 transition cursor-pointer">
             <User className="w-10 h-10 text-blue-600"/>
